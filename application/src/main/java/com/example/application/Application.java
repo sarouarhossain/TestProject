edd9165b5b19c64fbffe1ae286cebed18909200b
sarouarhossain <sarouar.hossain10@outlook.com>
2021-01-09 01:48:43 +0900

    test

--- application/src/main/java/com/example/application/Application.java
+++ application/src/main/java/com/example/application/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.common_library")
 public class Application {
 
 	public static void main(String[] args) {
--- application/src/main/java/com/example/application/TestController.java
+++ application/src/main/java/com/example/application/TestController.java
@@ -1,5 +1,7 @@
 package com.example.application;
 
+import com.example.common_library.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,10 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/")
 public class TestController {
+//    @Autowired
+//    TestService testService;
     @GetMapping("test")
     public String test(){
-        return "Test";
+        return "testService.getString()";
     }
 }
