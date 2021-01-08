4e4393d87f5c865ea314c7695c9952be2c9e83c8
sarouarhossain <sarouar.hossain10@outlook.com>
2021-01-09 02:01:46 +0900

    done

---
+++ application/src/main/java/com/example/application/HealthController.java
@@ -0,0 +1,12 @@
+package com.example.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
